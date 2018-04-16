cbd10333835877205c730b3f8f6898a67823e08c
Uri Schwartz <urischwartz@nat-tvwna-inside-visitornet101-a-29255.princeton.org>
2018-04-15 20:31:17 -0400

    reviews on admin page

--- clubtalk/page/admin.py
+++ clubtalk/page/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Club
 from .models import Category
 from .models import Leader
+from .models import Review
 
 admin.site.register(Category)
 
@@ -13,7 +14,10 @@ class LeaderAdmin(admin.ModelAdmin):
     list_display = ('name', 'title', 'email')
 admin.site.register(Leader, LeaderAdmin)
 
-# Register the Admin classes for Book using the decorator
 class ClubAdmin(admin.ModelAdmin):
     list_display = ('name', 'website', 'desc', 'display_cat', 'display_leaders')
 admin.site.register(Club, ClubAdmin)
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('text',)
+admin.site.register(Review, ReviewAdmin)
